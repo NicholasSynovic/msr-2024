413bab2f9c335ae19a13afb91b836a73fe5238c0
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-11-02 10:26:57 -0500

    Add code for generating the figure

--- msr_2024/createSankeyDiagram.py
+++ msr_2024/createSankeyDiagram.py
@@ -3,6 +3,26 @@ from typing import Any, List
 import click
 import pandas
 from pandas import DataFrame
+from plotly import graph_objects as go
+
+
+def createFigure(data: dict[str, List[Any]]) -> None:
+    fig = go.Figure(
+        data=[
+            go.Sankey(
+                node=dict(
+                    pad=15,
+                    thickness=20,
+                    line=dict(color="black", width=0.5),
+                    label=data["label"],
+                ),
+                link=data,
+            )
+        ]
+    )
+
+    fig.update_layout(title_text="HF License to GH License Mapping", font_size=10)
+    fig.show()
 
 
 def preprocessData(df: DataFrame) -> dict[str, List[Any]]:
@@ -29,7 +49,7 @@ def preprocessData(df: DataFrame) -> dict[str, List[Any]]:
     value: List[int] = [1] * len(source)
 
     data: dict[str, List[Any]] = {
-        "labels": labels,
+        "label": labels,
         "source": source,
         "target": target,
         "value": value,
@@ -51,40 +71,8 @@ def main(data_filepath: str) -> None:
 
     data: dict[str, List[Any]] = preprocessData(df=df)
 
-    from pprint import pprint as print
-
-    print(data)
+    createFigure(data=data)
 
 
 if __name__ == "__main__":
     main()
-    quit()
-
-    gh_indices = [unique_gh_license.index(lic) for lic in filtered_gh_license]
-    gh_indices = [
-        i + len(unique_hf_license) for i in gh_indices
-    ]  # offset indices for GH licenses
-
-    # Prepare data for the Sankey diagram
-    labels = unique_hf_license + unique_gh_license
-    source = hf_indices
-    target = gh_indices
-    value = [1] * len(source)  # assuming 1-1 mapping, each connection has a value of 1
-
-    # Create the Sankey diagram
-    fig = go.Figure(
-        data=[
-            go.Sankey(
-                node=dict(
-                    pad=15,
-                    thickness=20,
-                    line=dict(color="black", width=0.5),
-                    label=labels,
-                ),
-                link=dict(source=source, target=target, value=value),
-            )
-        ]
-    )
-
-    fig.update_layout(title_text="HF License to GH License Mapping", font_size=10)
-    fig.show()
