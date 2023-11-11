68861136026095763ac6655f718b08e8b348edbe
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-11-11 11:17:49 -0600

    Read and merge tables from database

---
+++ msr_2024/hfRepos/extractLicenses.py
@@ -0,0 +1,52 @@
+import sqlite3
+from sqlite3 import Connection
+
+import pandas
+from pandas import DataFrame
+
+
+def mergeTables(
+    models: DataFrame, licenses: DataFrame, modelLicensePairs: DataFrame
+) -> DataFrame:
+    licenses["name"].replace(to_replace="", value="None", inplace=True)
+    licenses.rename(
+        columns={"id": "license_id", "name": "license_name"},
+        inplace=True,
+    )
+
+    models.rename(columns={"id": "model_id"}, inplace=True)
+
+    foo: DataFrame = licenses.merge(
+        right=modelLicensePairs,
+        how="left",
+        on="license_id",
+    )
+    bar: DataFrame = foo.merge(right=models, how="left", on="model_id")
+
+    return bar
+
+
+def loadTable(table: str, con: Connection) -> DataFrame:
+    query: str = "SELECT * FROM {}"
+    df: DataFrame = pandas.read_sql_query(sql=query.format(table), con=con)
+    return df
+
+
+def main() -> None:
+    con: Connection = sqlite3.connect(database="../../data/PeaTMOSS.db")
+
+    models: DataFrame = loadTable(table="model", con=con)
+    licenses: DataFrame = loadTable(table="license", con=con)
+    modelLicensePairs: DataFrame = loadTable(table="model_to_license", con=con)
+
+    con.close()
+
+    df: DataFrame = mergeTables(
+        models=models,
+        licenses=licenses,
+        modelLicensePairs=modelLicensePairs,
+    )
+
+
+if __name__ == "__main__":
+    main()
