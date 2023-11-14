9c90dd1df017775a85b4cebd34cf6daca8f0f9c3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-11-13 22:40:10 -0600

    Create code

--- msr_2024/ghRepos/filesPerLicense.py
+++ msr_2024/ghRepos/filesPerLicense.py
@@ -1,16 +1,19 @@
 from json import load
+from os import listdir
 from pathlib import Path
-from typing import Any
+from pprint import pprint
+from typing import Any, List
 
 import pandas
 from pandas import DataFrame
 from progress.bar import Bar
 
+# IGNORE_FILE_PATTERNS: str = ".py|.sh|.html|.js|.pdf|Dockerfile|.tex|.pkl|.tar"
+CONDIENCE_THRESHOLD: int = 98
 
-def loadJSONFile(filepath: Path) -> DataFrame:
-    with open(
-        file="../../data/ghLicenses/facebookresearch_DisCo.json", mode="r"
-    ) as jsonFile:
+
+def loadJSONFile(filepath: Path) -> DataFrame | None:
+    with open(file=filepath, mode="r") as jsonFile:
         json: dict[str, Any] = load(jsonFile)
         jsonFile.close()
 
@@ -29,15 +32,44 @@ def loadJSONFile(filepath: Path) -> DataFrame:
         inplace=True,
     )
 
-    df: DataFrame = df[df["path"].str.contains(".py|.sh") == False,].reset_index(
-        drop=True
-    )
+    df["project"] = filepath.stem
+
+    try:
+        df: DataFrame = df[
+            df["percentage_of_license_text"] >= CONDIENCE_THRESHOLD
+        ].reset_index(drop=True)
+    except KeyError:
+        return None
 
     return df
 
 
+def printFilenames(df: DataFrame) -> None:
+    pathStrs: List[str] = df["path"].to_list()
+    paths: List[Path] = [Path(p) for p in pathStrs]
+
+    filenames: List[str] = [p.name for p in paths]
+
+    pprint(filenames)
+
+
 def main() -> None:
-    pass
+    dfs: List[DataFrame] = []
+    files: List[Path] = [
+        Path("../../data/ghLicenses", file)
+        for file in listdir(path="../../data/ghLicenses")
+    ]
+
+    with Bar(message="Loading JSON data... ", max=len(files)) as bar:
+        file: Path
+        for file in files:
+            dfs.append(loadJSONFile(filepath=file))
+            bar.next()
+
+    df: DataFrame = pandas.concat(objs=dfs, ignore_index=True)
+    df.sort_values(by="project", axis=0, inplace=True, ignore_index=True)
+
+    df.T.to_json(path_or_buf="ghProjectLicense.json", indent=4)
 
 
 if __name__ == "__main__":
